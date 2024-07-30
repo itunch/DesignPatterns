ad071648573d4af44ef00092f76a902d8ebf0f53
Aytunc ALPTEKIN <aytunc.alptekin@beymen.com>
2024-07-30 16:56:52 +0300

    Strategy Pattern initial MongoDb Connection String Added

---
+++ WebApp.Strategy/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Strategy.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [Key]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string UserId { get; set; } = string.Empty;
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
