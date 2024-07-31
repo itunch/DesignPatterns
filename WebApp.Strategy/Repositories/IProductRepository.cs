3b214d98ff370b9b6c45a741c697faef674e6aba
Aytunc ALPTEKIN <aytunc.alptekin@beymen.com>
2024-07-31 16:51:00 +0300

    Repositories added

---
+++ WebApp.Strategy/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetById(string Id);
+        Task<List<Product>> GetAllByUserId(string userId);
+        Task<Product> Save(Product product);
+        Task Update(Product product);
+        Task Delete(Product product);
+
+    }
+}
---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromSqlServer.cs
@@ -0,0 +1,43 @@
+﻿using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromSqlServer : IProductRepository
+    {
+        private readonly AppIdentityDbContext _context;
+        public ProductRepositoryFromSqlServer(AppIdentityDbContext appIdentityDbContext)
+        {
+            _context = appIdentityDbContext;
+        }
+        public async Task Delete(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllByUserId(string userId)
+        {
+            return await _context.Products.Where(p=>p.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product?> GetById(string Id)
+        {
+            return await _context.Products.FindAsync(Id);
+        }
+
+        public async Task<Product> Save(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
